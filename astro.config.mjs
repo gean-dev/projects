953b3ab392c0f169de8328f3eeecd1ae774c9e8e
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2023-05-20 10:39:45 +0700

    ✅ add icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://gean-dev.github.io/projects",
+  base: "/projects/",
   integrations: [tailwind()],
 });
